ded9446628975e195a8142e5f5d6849f092c26f1
felix043 <tranfelix99@gmail.com>
2022-09-30 09:37:12 +0200

    +300 XP Level up You have reached level 33

--- app/src/main/java/com/example/pythonOnAndroid/MainActivity.kt
+++ app/src/main/java/com/example/pythonOnAndroid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.pythonOnAndroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = this.findViewById(R.id.superiorText)
+
     }
 }
