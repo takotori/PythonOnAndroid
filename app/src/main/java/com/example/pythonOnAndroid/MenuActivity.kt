19bc543bc015e5caecb1306045351b73ff56a75d
gian-luca <gianluca.vogel@ost.ch>
2022-11-04 11:13:36 +0100

    Add ViewBinding

--- app/src/main/java/com/example/pythonOnAndroid/MenuActivity.kt
+++ app/src/main/java/com/example/pythonOnAndroid/MenuActivity.kt
@@ -1,11 +1,21 @@
 package com.example.pythonOnAndroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pythonOnAndroid.databinding.ActivityMenuBinding
+
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Todo change method in inside clickListener
+        binding.optionsBtn.setOnClickListener { binding.optionsBtn.text = "Clicked" }
+        binding.scoreBoardBtn.setOnClickListener { binding.scoreBoardBtn.text = "Clicked" }
+        binding.startGameBtn.setOnClickListener { binding.startGameBtn.text = "Clicked" }
     }
 }
