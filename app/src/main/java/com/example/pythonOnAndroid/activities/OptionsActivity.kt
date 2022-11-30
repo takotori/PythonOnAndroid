1fc6251a36bcf39cd86a2ba46f505eb3b31d9f7b
gian-luca <gianluca.vogel@ost.ch>
2022-11-30 11:39:34 +0100

    add ColorPickerDialog to Options

--- app/src/main/java/com/example/pythonOnAndroid/activities/OptionsActivity.kt
+++ app/src/main/java/com/example/pythonOnAndroid/activities/OptionsActivity.kt
@@ -3,6 +3,8 @@ package com.example.pythonOnAndroid.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pythonOnAndroid.databinding.ActivityOptionsBinding
+import com.github.dhaval2404.colorpicker.ColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
 
 class OptionsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOptionsBinding
@@ -11,5 +13,33 @@ class OptionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOptionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.optionsSnakeColorBtn.setOnClickListener{
+            ColorPickerDialog
+                .Builder(this)
+                .setTitle("Pick Snake Color")
+                .setColorShape(ColorShape.CIRCLE)
+                .setDefaultColor("#00FF00")
+                .setColorListener { color, colorHex ->
+                    binding.optionsSnakeColorBtn.setBackgroundColor(color)
+                }
+                .show()
+        }
+
+        binding.optionsFoodColorBtn.setOnClickListener{
+            ColorPickerDialog
+                .Builder(this)
+                .setTitle("Pick Food Color")
+                .setColorShape(ColorShape.CIRCLE)
+                .setDefaultColor("#FF0000")
+                .setColorListener { color, colorHex ->
+                    binding.optionsFoodColorBtn.setBackgroundColor(color)
+                }
+                .show()
+        }
+
+
     }
+
+
 }
