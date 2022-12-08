ab5e3d81c7b1e4aed1627a9ea6063b830faa59e3
felix043 <tranfelix99@gmail.com>
2022-12-08 16:48:50 +0100

    Reduce spawn area of food

--- app/src/main/java/com/example/pythonOnAndroid/gameObjects/Food.kt
+++ app/src/main/java/com/example/pythonOnAndroid/gameObjects/Food.kt
@@ -2,12 +2,12 @@ package com.example.pythonOnAndroid.gameObjects
 
 class Food {
     companion object {
-        var posX = (1..20).random().toFloat() * 50
-        var posY = (1..20).random().toFloat() * 50
+        var posX = (2..19).random().toFloat() * 50
+        var posY = (2..19).random().toFloat() * 50
 
         fun generate() {
-            posX = (1..20).random().toFloat() * 50
-            posY = (1..20).random().toFloat() * 50
+            posX = (2..19).random().toFloat() * 50
+            posY = (2..19).random().toFloat() * 50
         }
     }
 }
